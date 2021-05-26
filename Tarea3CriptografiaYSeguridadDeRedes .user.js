78a9a12dde85629b1a170751ad57f86ef2aee2ff
Juan Jose Vergara <40677444+JJVergaraManzi@users.noreply.github.com>
2021-05-26 00:00:09 -0400

    Primer commit con los archivos iniciales

---
+++ Tarea3CriptografiaYSeguridadDeRedes .user.js
@@ -0,0 +1,31 @@
+// ==UserScript==
+// @name         Tarea3CriptografiaYSeguridadDeRedes
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Encriptación con crypto-js
+// @author       Juan José Vergara
+// @match        file:///C:/Users/juan2/Documents/cripto/Tarea3/*
+// @grant        none
+// @require      https://cdnjs.cloudflare.com/ajax/libs/crypto-js/4.0.0/crypto-js.min.js
+// ==/UserScript==
+
+(function() {
+    //Capturar las variables del html
+    var AEShtml = document.getElementsByClassName("AES");
+    var IVhtml = document.getElementsByClassName("iv");
+    var Keyhtml = document.getElementsByClassName("keys");
+
+    //Parseo de IV y Key
+    var IVParsed= CryptoJS.enc.Hex.parse(IVhtml[0].id);
+    var KeyParsed =CryptoJS.enc.Hex.parse(Keyhtml[0].id);
+    var cipher = CryptoJS.lib.CipherParams.create({
+            ciphertext: CryptoJS.enc.Base64.parse(AEShtml[0].id)
+        });
+    //Desencriptacion
+    var decrypted = CryptoJS.AES.decrypt(cipher, KeyParsed,{
+        iv: IVParsed,
+        mode: CryptoJS.mode.CFB,
+        padding: CryptoJS.pad.ZeroPadding
+    });
+    alert(decrypted.toString(CryptoJS.enc.Utf8));
+})();
---
+++ index.py
@@ -0,0 +1,46 @@
+from base64 import b64encode
+from Crypto.Cipher import AES
+from Crypto.Random import get_random_bytes
+import binascii
+
+# inicialización de los datos
+textoPlano = 'EsteEsUnMensajeQueSeraEncriptado'
+keys = '1b69b49d6997ad001b69b49d69b97d00'
+iv   = "12345678123456781234123401972634"
+ivHex =binascii.a2b_hex(iv)
+#Keys a hexadecimal
+keysHex = binascii.a2b_hex(keys)
+
+cifrado = AES.new(keysHex, AES.MODE_CFB, iv= ivHex)
+
+#Encriptacion
+textoCifradoBytes =cifrado.encrypt(textoPlano.encode('utf-8'))
+textoCifrado = binascii.b2a_base64(textoCifradoBytes).rstrip().decode('utf-8')
+print("texto cifrado: " ,textoCifrado)
+
+#Variables para html
+textoCifradoHTML ='"'+textoCifrado+'"'
+keysHTML = '"'+keys+'"'
+ivHTML = '"'+iv+'"'
+
+#Archivo html
+html = open('index.html','w')
+message = """<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Tarea 3 Criptografia y seguridad en redes</title>
+    </head>
+    <body>
+        <p>Este sitio contiene un mensaje secreto</p>
+        <div class="AES" id="""+textoCifradoHTML+"""></div>
+        <div class="keys" id="""+keysHTML+"""></div>
+        <div class="iv" id="""+ivHTML+"""></div>
+    </body>
+</html>
+"""
+print(message)
+html.write(message)
+html.close()
